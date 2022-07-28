5a5c701e4a395e0c8f8c9f0528dcedcc81448f47
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2022-07-28 16:44:25 -0600

    fix: fixed logic

--- solution.mjs
+++ solution.mjs
@@ -1,52 +1,65 @@
 import axios from 'axios';
+
 const baseUrl = 'http://api.nbp.pl/api/cenyzlota';
-
+const investmentAvailable = 135000;
 const currentDate = new Date();
-
 let pricesLastFiveYears = [];
-
-// const bestInvestment = { bestBuy: '', bestSell: '', profit: ''}
 
 const fetchPricesByDates = async (startDate, endDate) => {
   const response = (await axios.get(`${baseUrl}/${startDate}/${endDate}`)).data;
 
-  pricesLastFiveYears = [...response, ...pricesLastFiveYears]
+  pricesLastFiveYears = [...response, ...pricesLastFiveYears];
 };
 
 const getPricesFor5Years = async () => {
   // 1 year ago
-  let startDate = `${currentDate.getFullYear()-1}-${currentDate.toISOString().slice(5, 10)}`;
+  let startDate = `${currentDate.getFullYear() - 1}-${currentDate
+    .toISOString()
+    .slice(5, 10)}`;
   let endDate = currentDate.toISOString().slice(0, 10);
   await fetchPricesByDates(startDate, endDate);
 
   // 2 years ago
   endDate = startDate;
-  startDate = `${+startDate.slice(0, 4) - 1}-${currentDate.toISOString().slice(5, 10)}`;
+  startDate = `${+startDate.slice(0, 4) - 1}-${currentDate
+    .toISOString()
+    .slice(5, 10)}`;
   await fetchPricesByDates(startDate, endDate);
 
   // 3 years ago
   endDate = startDate;
-  startDate = `${+startDate.slice(0, 4) - 1}-${currentDate.toISOString().slice(5, 10)}`;
+  startDate = `${+startDate.slice(0, 4) - 1}-${currentDate
+    .toISOString()
+    .slice(5, 10)}`;
   await fetchPricesByDates(startDate, endDate);
 
   // 4 years ago
   endDate = startDate;
-  startDate = `${+startDate.slice(0, 4) - 1}-${currentDate.toISOString().slice(5, 10)}`;
+  startDate = `${+startDate.slice(0, 4) - 1}-${currentDate
+    .toISOString()
+    .slice(5, 10)}`;
   await fetchPricesByDates(startDate, endDate);
 
   // 5 years ago
   endDate = startDate;
-  startDate = `${+startDate.slice(0, 4) - 1}-${currentDate.toISOString().slice(5, 10)}`;
+  startDate = `${+startDate.slice(0, 4) - 1}-${currentDate
+    .toISOString()
+    .slice(5, 10)}`;
   await fetchPricesByDates(startDate, endDate);
+};
 
-}
-
-getPricesFor5Years().then((result) => {
-  let minPrice = {price: 100000000000, date: '0'};
-  let maxPrice = {price: -10000000000, date: '0'};
+getPricesFor5Years().then((_result) => {
+  let minPrice = { price: 100000000000, date: '0' };
+  let maxPrice = { price: -10000000000, date: '0' };
+  let margin = 0,
+    profit = 0,
+    goldBoughtInGrams;
 
   pricesLastFiveYears.forEach((item) => {
-    if (item.cena < minPrice.price ) {
+    const minPriceDate = new Date(minPrice.date.slice(0, 4));
+    const maxPriceDate = new Date(maxPrice.date.slice(0, 4));
+
+    if (item.cena < minPrice.price) {
       minPrice.price = item.cena;
       minPrice.date = item.data;
     }
@@ -54,10 +67,25 @@ getPricesFor5Years().then((result) => {
     if (item.cena > maxPrice.price) {
       maxPrice.price = item.cena;
       maxPrice.date = item.data;
+
+      const currentMargin = maxPrice.price - minPrice.price;
+
+      if (currentMargin > margin && maxPriceDate > minPriceDate) {
+        margin = maxPrice.price - minPrice.price;
+        goldBoughtInGrams = investmentAvailable / minPrice.price;
+        profit = (goldBoughtInGrams * maxPrice.price).toFixed(2);
+      }
     }
   });
 
-  const bestInvestment = { bestBuy: minPrice.date, bestSell: maxPrice.date, profit: 0, margin: maxPrice.price - minPrice.price};
+  const bestInvestment = {
+    bestBuy: minPrice.date,
+    bestSell: maxPrice.date,
+    profit: `$${profit}`,
+    margin: `$${margin}`,
+  };
 
-  console.log(bestInvestment)
+  console.log(JSON.stringify(bestInvestment));
+
+  return JSON.stringify(bestInvestment);
 });
