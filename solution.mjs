3e8f781b74cb7a09497e9ad12a7f58d71452c94e
StarSheriff <61250665+StarSheriff2@users.noreply.github.com>
2022-07-28 12:06:58 -0600

    create README and edit solution

--- solution.mjs
+++ solution.mjs
@@ -2,14 +2,41 @@ import axios from 'axios';
 
 const baseUrl = 'http://api.nbp.pl/api/cenyzlota';
 
-const currentDate = new Date().toISOString().slice(0, 10);
+const currentDate = new Date();
 
-const fetchPrices = async () => {
-  const result = await (await axios.get(baseUrl)).data;
+const pricesLastFiveYears = [];
 
- console.log(result)
+const fetchPricesByDates = async (startDate, endDate) => {
+  console.log({startDate, endDate});
+  const result = await (await axios.get(`${baseUrl}/${startDate}/${endDate}`)).data;
+
+  pricesLastFiveYears.push(...result)
+  console.log(pricesLastFiveYears)
 };
 
-fetchPrices();
+// 1 year ago
+let startDate = `${currentDate.getFullYear()-1}-${currentDate.toISOString().slice(6, 10)}`;
+let endDate = currentDate.toISOString().slice(0, 10);
+fetchPricesByDates(startDate, endDate);
 
-console.log(currentDate)
+// // 2 years ago
+// startDate = `${+startDate - 1}-${currentDate.toISOString().slice(6, 10)}`;
+// endDate = startDate;
+// fetchPricesByDates(startDate, endDate);
+
+// // 3 years ago
+// startDate = `${+startDate - 1}-${currentDate.toISOString().slice(6, 10)}`;
+// endDate = startDate;
+// fetchPricesByDates(startDate, endDate);
+
+// // 4 years ago
+// startDate = `${+startDate - 1}-${currentDate.toISOString().slice(6, 10)}`;
+// endDate = startDate;
+// fetchPricesByDates(startDate, endDate);
+
+// // 5 years ago
+// startDate = `${+startDate - 1}-${currentDate.toISOString().slice(6, 10)}`;
+// endDate = startDate;
+// fetchPricesByDates(startDate, endDate);
+
+console.log('final array: ', pricesLastFiveYears);
